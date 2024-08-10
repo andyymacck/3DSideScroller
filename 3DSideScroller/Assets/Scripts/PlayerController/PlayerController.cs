fbde2d7eb1934d3ed5f29275106fa18d6bdc0be1
Andrew Mackay <andyymackay@gmail.com>
2024-08-10 13:49:51 -0400

    jump and movement

--- 3DSideScroller/Assets/Scripts/PlayerController/PlayerController.cs
+++ 3DSideScroller/Assets/Scripts/PlayerController/PlayerController.cs
@@ -1,18 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Rigidbody m_rigidbody;
+    [SerializeField] private float m_speedMovement = 2f;
+    [SerializeField] private float m_speedJump = 2f;
+
+    private void Awake()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Move(false);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Move(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        Vector3 moveDir = Vector3.up;
+        m_rigidbody.AddForce(moveDir * m_speedJump);
+    }
+
+
+    private void Move(bool isLeft)
+    {
+        Vector3 moveDir = isLeft ? Vector3.left : Vector3.right;
+        m_rigidbody.AddForce(moveDir * m_speedMovement);
     }
 }
