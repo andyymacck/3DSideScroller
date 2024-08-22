1e25f80580650e3978ffc9b85b2eabc3ec418837
Andrew Mackay <andyymackay@gmail.com>
2024-08-21 21:57:58 -0400

    Platform Collisions

---
+++ 3DSideScroller/Assets/Scripts/PlayerController/OneWayCollider.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class OneWayCollider : MonoBehaviour
+{
+    [SerializeField] private Collider m_collider;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ChangeColissionState(other, true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ChangeColissionState(other, false);
+        }
+    }
+
+    private void ChangeColissionState(Collider other, bool isEnable)
+    {
+        Physics.IgnoreCollision(other, m_collider, isEnable);
+    }
+}
--- 3DSideScroller/Assets/Scripts/PlayerController/PlayerController.cs
+++ 3DSideScroller/Assets/Scripts/PlayerController/PlayerController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody m_rigidbody;
-    [SerializeField] private float m_speedMovement = 2f;
-    [SerializeField] private float m_speedJump = 2f;
+    [SerializeField] private float m_forceMovement = 2f;
+    [SerializeField] private float m_forceJump = 2f;
 
     private void Awake()
     {
@@ -19,32 +19,43 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
             Move(false);
+
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
             Move(true);
         }
 
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
         {
             Jump();
         }
+
     }
 
     private void Jump()
     {
         Vector3 moveDir = Vector3.up;
-        m_rigidbody.AddForce(moveDir * m_speedJump);
+        m_rigidbody.AddForce(moveDir * m_forceJump, ForceMode.Impulse);
     }
-
 
     private void Move(bool isLeft)
     {
         Vector3 moveDir = isLeft ? Vector3.left : Vector3.right;
-        m_rigidbody.AddForce(moveDir * m_speedMovement);
+        m_rigidbody.AddForce(moveDir * m_forceMovement);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log($"[OnTriggerExit] Name:{collision.gameObject.name}, Tag:{collision.gameObject.tag}");
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        //Debug.Log($"[OnTriggerExit] Name:{collision.gameObject.name}, Tag:{collision.gameObject.tag}");
     }
 }
